0e01d1d9e3bf1d0a911ce21846d6ee9b2e769254
Duong <buiducduong1@gmail.com>
2024-06-14 10:15:03 +0700

    chua bai tapj

--- lession22/ex03.js
+++ lession22/ex03.js
@@ -8,19 +8,24 @@ export function getMinMissingPositiveIntegers(arr) {
 		i++
 	}
 
-	var result = 1
+	if (arr[i] != 1) {
+		return 1
+	}
 
-	while (i < arr.length) {
-		if (arr[i] === result) {
-			i++
-			result++
-		} else if (arr[i] === result - 1) {
+	while (i + 1 < arr.length) {
+		if (arr[i + 1] === arr[i] || arr[i + 1] === arr[i] + 1) {
 			i++
 		} else {
-			return result
+			return arr[i] + 1
 		}
 	}
-	return result
+	return arr[arr.length - 1] + 1
 }
 
 console.log(getMinMissingPositiveIntegers([1, 1, 1, 1, 2, 3, 5])) //4
+console.log(getMinMissingPositiveIntegers([-1, 1, 4, 5])) //4
+console.log(getMinMissingPositiveIntegers([-1, 1, 2, 3])) //4
+console.log(getMinMissingPositiveIntegers([0, 2, 3, 4])) //1
+console.log(getMinMissingPositiveIntegers([2, 3, 4])) //1
+console.log(getMinMissingPositiveIntegers([-3, -2, 3, 4])) //1
+console.log(getMinMissingPositiveIntegers([-3, 1, 2, 3, 3, 4])) //5
