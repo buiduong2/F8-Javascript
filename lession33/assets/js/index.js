691009864aad8a9d2bc53f983b444f2a8f159e5b
Duong <buiducduong1@gmail.com>
2024-07-27 16:41:56 +0700

    lession 33 correct exexercise

--- lession33/assets/js/index.js
+++ lession33/assets/js/index.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
 	// Format Action Handle
-	var contentEl = document.querySelector('.editor-content')
+	var contenteditableEl = document.querySelector('.editor-content')
 	var quantityLetterEl = document.querySelector('.quantity--letter .quantity')
 	var quantityWordEl = document.querySelector('.quantity--word .quantity')
-    var btnClear = document.querySelector('.btn--clear')
-	var formatBold = new FormatBtn('.btn--bold', 'bold', contentEl)
-	var formatUnderline = new FormatBtn( '.btn--underline', 'underline', contentEl)
-	var formatItalic = new FormatBtn('.btn--italic', 'italic', contentEl)
-	var formatColor = new FormatColorBtn('.btn--color', 'foreColor', contentEl)
+	var btnClear = document.querySelector('.btn--clear')
+	var inputFileNameEl = document.querySelector('.input-file-name')
+
+	var formatBold = new FormatBtn('.btn--bold', 'bold', contenteditableEl)
+	var formatUnderline = new FormatBtn(
+		'.btn--underline',
+		'underline',
+		contenteditableEl
+	)
+	var formatItalic = new FormatBtn(
+		'.btn--italic',
+		'italic',
+		contenteditableEl
+	)
+	var formatColor = new FormatColorBtn(
+		'.btn--color',
+		'foreColor',
+		contenteditableEl
+	)
 
 	var tagMap = {
 		B: formatBold,
@@ -21,36 +35,64 @@ document.addEventListener('DOMContentLoaded', function () {
 		i: formatItalic
 	}
 
+	//Add Event Listener cho tất cả các Button
 	Object.values(tagMap).forEach(function (formatBtn) {
 		formatBtn.addEventListener()
 	})
 
-    btnClear.addEventListener('click', function () {
-        contentEl.innerHTML = ''
-        quantityLetterEl.textContent = "0"
-		quantityWordEl.textContent = "0"
-        removeAllBtnActiveClass();
-        contentEl.focus()
-    })
+	btnClear.addEventListener('click', function () {
+		contenteditableEl.innerHTML = ''
+		quantityLetterEl.textContent = '0'
+		quantityWordEl.textContent = '0'
+		inputFileNameEl.value = 'untitled'
+		removeAllBtnActiveClass()
+		contenteditableEl.focus()
+	})
 
-	contentEl.addEventListener('click', addActiveClassBtnByCurrentSelect)
+	document.addEventListener('selectionchange', function (e) {
+		// Lắng nghe sự kiện khi contentedableEl's selection thay đổi
+		var node = getEditableElSelectedNode()
+		if (node != null) {
+			addActiveClassBtnByCurrentSelect(node)
+		}
+	})
 
-	contentEl.addEventListener('input', function (e) {
+	contenteditableEl.addEventListener('input', function (e) {
+		// Hành vi của JS một khi đã xuống dòng thì ko thể thoát ra một cách trực tiếp mà phải xóa 2 lần
+		if (this.innerText.length === 1 && this.innerText[0] === '\n') {
+			this.innerText = ''
+		}
 		quantityLetterEl.textContent = String(countLetter())
 		quantityWordEl.textContent = String(countWord())
 	})
 
-	contentEl.addEventListener('keydown', function (e) {
-		var moveCursorKeys = [ 'ArrowLeft', 'ArrowRight', 'ArrowTop', 'ArrowBottom', 'Delete', 'Backspace' ]
+	//Lắng nghe sự kiện sử dụng phím tắt để format
+	contenteditableEl.addEventListener('keydown', function (e) {
 		if (e.ctrlKey && e.key in keyBoardMap) {
 			keyBoardMap[e.key].toggleActive()
-		} else if (moveCursorKeys.includes(e.key)) {
-			addActiveClassBtnByCurrentSelect()
 		}
 	})
 
+	// prevent paste element behavior to contentediableEl
+	contenteditableEl.addEventListener('paste', function (e) {
+		e.preventDefault()
+		var text = (e.clipboardData || window.clipboardData).getData('text')
+		document.execCommand('insertText', false, text)
+	})
+
+	/**
+	 * - hàm Đếm số lượng từ
+	 * - Tiến hành loop qua từng character của nội dung InnerText và so sánh xem nó có phải là một kí tự:
+	 *  space, xuống dòng, tab ... hay không.
+	 * - có 2 TH:
+	 * + Nếu là một kí tự space . Ta đánh đấu là chuẩn bị bắt đầu một word. encounteredLetter = false;
+	 * + nếu là một kí tự Letter: Ta tiến hành count++. Đánh đấu là đã gặp word. ko đếm nữa encounteredLetter = true
+	 * @returns {number} Số lượng từ
+	 */
 	function countWord() {
-		var str = contentEl.innerText
+		var str = contenteditableEl.innerText
+
+		// CharCode theo mã ASCII của một số kí tự thuộc nhóm space - được coi là không phải là một kí tự
 		var nonLetterCharCodes = new Set([160, 10, 32, 9]) //['&npsb'; '\n', ' ' , '\t']
 		var encounteredLetter = false
 		var count = 0
@@ -68,33 +110,48 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	function countLetter() {
-		return contentEl.innerText.length
+		return contenteditableEl.innerText.length
 	}
 
-	function addActiveClassBtnByCurrentSelect() {
+	/**
+	 * B1: Traversel currentNode up to root = contentEl.  child -> parent-> parent's parent .
+	 * Nếu gặp ContentEl (editable elemnet) break;
+	 * B2: Kiểm tra xem ELement hiện tại có nằm trong nhóm các tag (b , i , font) đang theo dõi
+	 * - TH1: tag hiện tại nằm trong nhóm tag theo dõi. Tiến hành cài đặt lại thông số của btn thông qua method addActiveState(currentNode)
+	 * - TH2: ko phải thì tiến hành duyệt tiếp
+	 * @param {Node} selectedNode : node được Selected  và thuộc contenteditableEl
+	 * @returns {void}
+	 */
+	function addActiveClassBtnByCurrentSelect(selectedNode) {
 		removeAllBtnActiveClass()
-		var node = getSelectedNode()
 
-		if (node === null) return
+		if (selectedNode === null) return
 
-		while (node !== contentEl) {
-			if (node instanceof HTMLElement && node.tagName in tagMap) {
-				tagMap[node.tagName].addActiveState(node)
+		while (selectedNode !== contenteditableEl) {
+			if (
+				selectedNode instanceof HTMLElement &&
+				selectedNode.tagName in tagMap
+			) {
+				tagMap[selectedNode.tagName].addActiveState(selectedNode)
 			}
-			node = node.parentElement
+			selectedNode = selectedNode.parentElement
 		}
 	}
 
-	function getSelectedNode() {
-		var selectedNode = window.getSelection()
+	/**
+	 *- Lấy ra Node tổ tiên gần nhất của tất cả các Node được bôi đen
+	 * @returns {Node | null}
+	 *
+	 * - TH1- null:  Elemetn hiện tại đang active (đang forcus ) ko phải là editable element return null
+	 * - TH2- Node: trả về cha chung gần nhất của tất cả các Node được select các phần tử trong ContentEdable El;
+	 */
+	function getEditableElSelectedNode() {
+		var selection = window.getSelection()
 
-		if (document.activeElement !== contentEl) return null
-		if (!selectedNode || selectedNode.rangeCount === 0) return null
-        
-		var startContainer = selectedNode.getRangeAt(0).startContainer
-		var endContainer = selectedNode.getRangeAt(0).endContainer
+		if (document.activeElement !== contenteditableEl) return null
+		if (!selection || selection.rangeCount === 0) return null
 
-		return startContainer === endContainer ? startContainer : null
+		return selection.getRangeAt(0).commonAncestorContainer
 	}
 
 	function removeAllBtnActiveClass() {
@@ -103,17 +160,15 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
-    
 	// File Handler
 	window.addEventListener('load', function () {
 		var btnSaveTxt = document.querySelector('.btn--save-txt')
 		var btnSavePdf = document.querySelector('.btn--save-pdf')
-		var inputFileNameEl = document.querySelector('.input-file-name')
-
-
 
 		btnSaveTxt.addEventListener('click', function () {
-			var blob = new Blob([contentEl.innerText], { type: 'text/plain' })
+			var blob = new Blob([contenteditableEl.innerText], {
+				type: 'text/plain'
+			})
 			var url = URL.createObjectURL(blob)
 			goUrl(url)
 			URL.revokeObjectURL(url)
@@ -124,7 +179,7 @@ document.addEventListener('DOMContentLoaded', function () {
 				margin: 1,
 				filename: inputFileNameEl.value
 			}
-			html2pdf(contentEl, opt)
+			html2pdf(contenteditableEl, opt)
 		})
 
 		function goUrl(url) {
