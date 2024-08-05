c9d3a761cb287902d9d2f4ad1e5ecca8c336e3a2
Duong <buiducduong1@gmail.com>
2024-08-05 20:30:38 +0700

    lession36 js done

---
+++ lession36/assets/js/index.js
@@ -0,0 +1,174 @@
+"use strict";
+var SpeechRecognition = (window.webkitSpeechRecognition);
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'vi-VN';
+recognition.continuous = false;
+recognition.interimResults = true;
+
+const speechContentDetailEl = document.querySelector(".search-content-detail");
+const msgWrapperEl = document.querySelector(".msg-wrapper");
+const speechContentWrapperEl = document.querySelector(".search-content-wrapper");
+const btnEl = document.querySelector(".btn");
+
+let speechContent = "";
+
+function changeClassState(newState, el) {
+    el.classList.remove("idle");
+    el.classList.remove("failure");
+    el.classList.remove("pending");
+    el.classList.remove("success");
+    el.classList.add(newState);
+}
+
+btnEl.onclick = function () {
+    changeClassState("pending", msgWrapperEl);
+    changeClassState("pending", speechContentWrapperEl);
+    recognition.start();
+};
+
+recognition.onspeechend = function () {
+    recognition.stop();
+};
+
+recognition.onresult = function (e) {
+    let msg = Array.from(e.results)
+        .map(result => result[0])
+        .map(alter => alter.transcript)
+        .join("");
+    speechContentDetailEl.innerHTML = msg;
+    speechContent = msg;
+};
+
+recognition.onend = function () {
+    proccessSpeechContent(speechContent)
+        .then((url) => {
+        changeClassState("success", msgWrapperEl);
+        changeClassState("success", speechContentWrapperEl);
+        setTimeout(() => {
+            goUrl(url);
+        }, 1000);
+    })
+        .catch(() => {
+        changeClassState("failure", msgWrapperEl);
+        changeClassState("failure", speechContentWrapperEl);
+    })
+        .finally(() => {
+        setTimeout(() => {
+            changeClassState("idle", msgWrapperEl);
+            changeClassState("idle", speechContentWrapperEl);
+            speechContentDetailEl.textContent = "";
+        }, 2000);
+    });
+};
+
+// di chuyển đến link tương ứng
+function goUrl(url) {
+    const aEl = document.createElement("a");
+    aEl.href = url;
+    aEl.target = "_blank";
+    document.body.append(aEl);
+    aEl.click();
+    aEl.remove();
+}
+
+const services = [
+    {
+        keywords: ['google maps', 'google map'],
+        url: "https://www.google.com/maps"
+    },
+    {
+        keywords: ['google drive'],
+        url: "https://drive.google.com/"
+    },
+    {
+        keywords: ['google'],
+        url: "https://www.google.com.vn/",
+    },
+    {
+        keywords: ['facebook'],
+        url: 'https://www.facebook.com/'
+    },
+    {
+        keywords: ['youtube'],
+        url: 'https://www.youtube.com/'
+    },
+    {
+        keywords: ['chỉ đường tới', 'chỉ đường', 'đường tới', 'tới'],
+        url: "https://www.google.com/maps/search/",
+        encodeChars: {
+            ' ': '%20'
+        }
+    },
+    {
+        keywords: ['bài hát', 'mở bài hát', ' nghe bài hát'],
+        url: 'https://zingmp3.vn/tim-kiem/tat-ca?q=',
+        encodeChars: {
+            ' ': "+"
+        }
+    },
+    {
+        keywords: ['xem video', 'mở video', 'video', 'phim', 'xem'],
+        url: "https://www.youtube.com/results?search_query=",
+        encodeChars: {
+            ' ': '+'
+        }
+    }
+];
+// Xử lý nội dung giọng nói vừa được nhận diện
+function proccessSpeechContent(speechContent) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const serviceMatch = getService(speechContent);
+            if (serviceMatch == null) {
+                reject("Error");
+                return;
+            }
+            const { service, keyword } = serviceMatch;
+            let orderDetail = getOrderDetail(keyword, speechContent);
+            Object.entries(service.encodeChars || {})
+                .forEach(([key, value]) => {
+                orderDetail = orderDetail.replace(key, value);
+            });
+            const url = service.url + orderDetail;
+            resolve(url);
+        }, 1500);
+    });
+}
+
+// lấy ra server có độ khớp cao nhất
+function getService(speechContent) {
+    speechContent = speechContent.toLowerCase();
+    const serviceMatches = [];
+    for (const service of services) {
+        for (const keyword of service.keywords) {
+            const index = speechContent.indexOf(keyword);
+            if (index != -1) {
+                serviceMatches.push({ service, keyword, index });
+            }
+        }
+    }
+    if (serviceMatches.length == 0) {
+        return null;
+    }
+    const bestMatch = serviceMatches.reduce((bestMatch, current) => {
+        if (bestMatch.index != current.index) {
+            return bestMatch.index < current.index ? bestMatch : current;
+        }
+        return bestMatch.keyword.length > current.keyword.length ? bestMatch : current;
+    });
+    return bestMatch;
+}
+
+// lấy ra thông tin về nội dung của yêu cầu
+function getOrderDetail(keyword, speechContent) {
+    const index = speechContent.toLocaleLowerCase().indexOf(keyword);
+    if (index == -1)
+        return "";
+    if (index + keyword.length < speechContent.length) {
+        return speechContent.substring(index + keyword.length).trim();
+    }
+    return speechContent.substring(0, index).trim();
+}
+
+
