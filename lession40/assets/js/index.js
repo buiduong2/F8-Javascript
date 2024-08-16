cd9ee21a2ce89e047e04fc01933da88f7397ee5b
Duong <buiducduong1@gmail.com>
2024-08-16 11:34:17 +0700

    add remove Toast

--- lession40/assets/js/index.js
+++ lession40/assets/js/index.js
@@ -1,68 +1,74 @@
-"use strict";
-const postListEl = document.querySelector(".post-list");
-const postListEnd = document.querySelector(".post-list-end");
-const loaderEl = document.querySelector(".loader");
-addInfinityScrollEvent();
+'use strict'
+const postListEl = document.querySelector('.post-list')
+const postListEnd = document.querySelector('.post-list-end')
+const loaderEl = document.querySelector('.loader')
+addInfinityScrollEvent()
 function addInfinityScrollEvent() {
-    const pageQuery = {
-        _page: 1,
-        _limit: 4,
-    };
-    let postCount = 0;
-    let isFetching = false;
-    const overserver = new IntersectionObserver((entries) => {
-        entries.forEach(async (entry) => {
-            if (entry.isIntersecting) {
-                if (isFetching)
-                    return;
-                try {
-                    isFetching = true;
-                    loaderEl.classList.add("loading");
-                    const { res, postLength } = await fetchPosts(pageQuery);
-                    postCount = postCount + postLength;
-                    const totalCount = parseInt(res.headers.get('x-total-count') || '0');
-                    if (postCount < totalCount) {
-                        pageQuery._page++;
-                    }
-                    else {
-                        showToast("All Post has been successfully loaded");
-                        overserver.unobserve(postListEnd);
-                    }
-                }
-                catch (error) {
-                    showToast("An error occurred on the server. Please try again later. " + String(error));
-                    overserver.observe(postListEnd);
-                }
-                finally {
-                    isFetching = false;
-                    loaderEl.classList.remove("loading");
-                }
-            }
-        });
-    }, {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.9
-    });
-    overserver.observe(postListEnd);
+	const pageQuery = {
+		_page: 1,
+		_limit: 4
+	}
+	let postCount = 0
+	let isFetching = false
+	const overserver = new IntersectionObserver(
+		entries => {
+			entries.forEach(async entry => {
+				if (entry.isIntersecting) {
+					if (isFetching) return
+					try {
+						isFetching = true
+						loaderEl.classList.add('loading')
+						const { res, postLength } = await fetchPosts(pageQuery)
+						postCount = postCount + postLength
+						const totalCount = parseInt(
+							res.headers.get('x-total-count') || '0'
+						)
+						if (postCount < totalCount) {
+							pageQuery._page++
+						} else {
+							showToast('All Post has been successfully loaded')
+							overserver.unobserve(postListEnd)
+						}
+					} catch (error) {
+						showToast(
+							'An error occurred on the server. Please try again later. ' +
+								String(error)
+						)
+						overserver.observe(postListEnd)
+					} finally {
+						isFetching = false
+						loaderEl.classList.remove('loading')
+					}
+				}
+			})
+		},
+		{
+			root: null,
+			rootMargin: '0px',
+			threshold: 0.9
+		}
+	)
+	overserver.observe(postListEnd)
 }
 async function fetchPosts(query) {
-    const searchParam = new URLSearchParams(query);
-    const res = await fetch('https://stvp8n-8080.csb.app/posts?' + searchParam.toString());
-    if (!res.ok) {
-        throw new Error("Server Error");
-    }
-    const posts = await res.json();
-    const fragment = document.createDocumentFragment();
-    posts.map(createPost).forEach(el => fragment.appendChild(el));
-    postListEl.appendChild(fragment);
-    return { res, postLength: posts.length };
+	const searchParam = new URLSearchParams(query)
+	const res = await fetch(
+		'https://stvp8n-8080.csb.app/posts?' + searchParam.toString()
+	)
+	if (!res.ok) {
+		throw new Error('Server Error')
+	}
+	const posts = await res.json()
+	const fragment = document.createDocumentFragment()
+	posts.map(createPost).forEach(el => fragment.appendChild(el))
+	postListEl.appendChild(fragment)
+	return { res, postLength: posts.length }
 }
 function createPost(post) {
-    const user = post.user;
-    const el = document.createElement("div");
-    el.classList.add("post");
-    el.innerHTML = `
+	const user = post.user
+	const el = document.createElement('div')
+	el.classList.add('post')
+	el.innerHTML = `
                     <div class="user-info">
                         <a href="profile.html#profile-details" class="user-name">${user.name}</a>
 
@@ -93,14 +99,17 @@ function createPost(post) {
                     <div class="reactions">
                         <button class="btn-xsmall">+ <i class="fa fa-smile-o emoji"></i></button>
                     </div>
-                `;
-    return el;
+                `
+	return el
 }
 function showToast(msg) {
-    const toastPrototype = document.querySelector(".toast");
-    const clone = toastPrototype.cloneNode(true);
-    toastPrototype.insertAdjacentElement("afterend", clone);
-    clone.querySelector(".toast-body").innerText = msg;
-    const toast = bootstrap.Toast.getOrCreateInstance(clone);
-    toast.show();
+	const toastPrototype = document.querySelector('.toast')
+	const clone = toastPrototype.cloneNode(true)
+	toastPrototype.insertAdjacentElement('afterend', clone)
+	clone.querySelector('.toast-body').innerText = msg
+	const toast = bootstrap.Toast.getOrCreateInstance(clone)
+	toast.show()
+	clone.addEventListener('hidden.bs.toast', function (e) {
+		clone.remove()
+	})
 }
