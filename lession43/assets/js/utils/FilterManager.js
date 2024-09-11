f1eaa2988da8bfa5836d7e16cfe5c9cad7347205
Duong <buiducduong1@gmail.com>
2024-09-11 10:42:13 +0700

    lesson43 let embed youtube link be unique

--- lession43/assets/js/utils/FilterManager.js
+++ lession43/assets/js/utils/FilterManager.js
@@ -1,7 +1,7 @@
 export class FilterManager {
     constructor(providers) {
         this.map = new Map();
-        this.providers = providers.map(({ id, regexp, replacer }) => new FilterProvider(id, regexp, replacer));
+        this.providers = providers;
         this.validateProviders();
     }
     validateProviders() {
@@ -16,16 +16,9 @@ export class FilterManager {
             throw new Error("Type Error: Modifler's regexp of Provider must be Global");
         }
     }
-    /**
-     * - Tiến hành duyệt qua từng Filter một và chỉnh sửa lại nội dung của content
-     * Như là nhúng link nhúng video.
-     */
     applyFilter(content) {
-        // Lưu trữ nội dung . Lưu trữ vị trí thay đổi
         content = this.applyProviders(content);
-        // Tiến hành sửa đổi thực sự
         content = this.replaceAllContent(content);
-        // Xóa đi dữ liệu không còn cần thiết
         this.map = new Map();
         return content;
     }
@@ -39,7 +32,7 @@ export class FilterManager {
         }, content);
     }
     replaceAllContent(content) {
-        const regexp = /\$\$(\w+)\$\$/g;
+        const regexp = /\$\$(.+?)\$\$/g;
         return content.replaceAll(regexp, (match, g1) => {
             if (this.map.has(g1)) {
                 return this.map.get(g1);
@@ -49,10 +42,9 @@ export class FilterManager {
     }
 }
 class FilterProvider {
-    constructor(id, regexp, replacer) {
+    constructor(id, regexp) {
         this.id = id;
         this.regexp = regexp;
-        this.replacer = replacer;
     }
     applyReplaceAll(content) {
         let count = 0;
@@ -68,65 +60,82 @@ class FilterProvider {
         };
     }
 }
-function embedPhoneLink(phoneNumber) {
-    return `<a href="tel:${phoneNumber}">${phoneNumber}</a>`;
-}
-function embedEmailLink(email) {
-    const aEl = document.createElement("a");
-    aEl.href = `mailto:${email}`;
-    aEl.textContent = email;
-    return aEl.outerHTML;
-}
-function embedYoutubeLink(youtubeLink, id) {
-    return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}?si=CjtCwkca7pYIMeQ4"
-        title="YouTube video player" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
-}
-function embedLink(link) {
-    const aEl = document.createElement("a");
-    aEl.href = link;
-    aEl.target = "_blank";
-    aEl.textContent = link;
-    return aEl.outerHTML;
-}
-function removeTrailingSpace(mutilSpace) {
-    if (mutilSpace.includes("\n")) {
-        return "\n";
+class EmbedPhoneLink extends FilterProvider {
+    constructor() {
+        super("P", /(0|\+84)\d{9}/g);
     }
-    return " ";
+    replacer(phoneNumber) {
+        return `<a href="tel:${phoneNumber}">${phoneNumber}</a>`;
+    }
 }
-// Replacer là các hàm mà sẽ thay đổi nội dung của dữ liệu khớp tương ứng với regexp
+class EmbedEmailLink extends FilterProvider {
+    constructor() {
+        super("M", /\w(?=[^@]{3,29}@)((\.|-)\w+|\w+)*@[a-zA-Z0-9](-[a-zA-Z0-9]|[a-zA-Z0-9])+(\.[a-zA-Z0-9](-[a-zA-Z0-9]|[a-zA-Z0-9])+){1,2}/g);
+    }
+    replacer(email) {
+        const aEl = document.createElement("a");
+        aEl.href = `mailto:${email}`;
+        aEl.textContent = email;
+        return aEl.outerHTML;
+    }
+}
+class EmbedYoutubeLink extends FilterProvider {
+    constructor() {
+        super("Y", /https:\/\/(?:www\.youtube\.com\/watch\?v=([\w-]{11})[^\s]*|youtu\.be\/([\w-]{11})[^\s]*)/g);
+    }
+    applyReplaceAll(content) {
+        const idSet = new Set();
+        const changedContent = [];
+        content = content.replaceAll(this.regexp, (youtubeLink, id1, id2) => {
+            const yId = id1 ?? id2;
+            const id = this.id + "_" + yId;
+            if (idSet.has(id)) {
+                return "";
+            }
+            idSet.add(id);
+            changedContent.push({ id, target: this.replacer(youtubeLink, yId) });
+            return `$$${id}$$`;
+        });
+        return {
+            content,
+            changedContent
+        };
+    }
+    replacer(youtubeLink, id) {
+        return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+    }
+}
+class EmbedLink extends FilterProvider {
+    constructor() {
+        super("L", /https?:\/\/(www\.|ww2\.)?([\w-]+(\.[\w-]+)+)[^\s]*/g);
+    }
+    replacer(link) {
+        const aEl = document.createElement("a");
+        aEl.href = link;
+        aEl.target = "_blank";
+        aEl.textContent = link;
+        return aEl.outerHTML;
+    }
+}
+class RemoveTrailingSpace extends FilterProvider {
+    constructor() {
+        super("S", /\s{2,}/g);
+    }
+    replacer(mutilSpace) {
+        if (mutilSpace.includes("\n")) {
+            return "\n";
+        }
+        return " ";
+    }
+}
 const providers = [
-    {
-        id: "P",
-        regexp: /(0|\+84)\d{9}/g,
-        replacer: embedPhoneLink
-    },
-    {
-        id: "M",
-        regexp: /\w(?=[^@]{3,29}@)((\.|-)\w+|\w+)*@[a-zA-Z0-9](-[a-zA-Z0-9]|[a-zA-Z0-9])+(\.[a-zA-Z0-9](-[a-zA-Z0-9]|[a-zA-Z0-9])+){1,2}/g,
-        replacer: embedEmailLink
-    },
-    {
-        id: "YL",
-        regexp: /https:\/\/www\.youtube\.com\/watch\?v=([\w-]{11})[^\s]*/g,
-        replacer: embedYoutubeLink
-    },
-    {
-        id: "YS",
-        regexp: /https:\/\/youtu\.be\/([\w-]{11})[^\s]*/g,
-        replacer: embedYoutubeLink
-    },
-    {
-        id: "L",
-        regexp: /https?:\/\/(www\.|ww2\.)?([\w-]+(\.[\w-]+)+)[^\s]*/g,
-        replacer: embedLink
-    },
-    {
-        id: "S",
-        regexp: /\s{2,}/g,
-        replacer: removeTrailingSpace
-    },
+    new EmbedPhoneLink(),
+    new EmbedEmailLink(),
+    new EmbedYoutubeLink(),
+    new EmbedLink(),
+    new RemoveTrailingSpace(),
 ];
 export const embedManager = new FilterManager(providers);
